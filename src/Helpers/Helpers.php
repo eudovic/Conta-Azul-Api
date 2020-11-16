5df3f48b272b84889684aff2cba377018e2bc20b
Emerson Udovic <31167386+eudovic@users.noreply.github.com>
2020-11-16 17:23:19 -0300

    Update Helpers.php

--- src/Helpers/Helpers.php
+++ src/Helpers/Helpers.php
@@ -12,7 +12,7 @@ class Helpers{
       return $randomString;
   }
   public static function isJson($string) {
-    json_decode($string);
+    @json_decode($string);
     return (json_last_error() == JSON_ERROR_NONE);
   }
 }
